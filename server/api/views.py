1f8a5aa0bb43e60f1a280790285060ec07702f4d
Charith <114335319+Charith0401@users.noreply.github.com>
2024-06-01 01:04:06 +0530

    updated code so that serializers.serialize is not needed instead .values() is adopted with the result of queries being converted into a list before sent over as a jsonresponse

--- server/api/views.py
+++ server/api/views.py
@@ -8,35 +8,46 @@ import json
 from api.models import Rates
 import datetime
 
-# Create your views here.
-
-#you can import "Q" from django models and this basically allows you to do OR operations
-
 @api_view(["GET"])
 def viewAllRates(request):
     if (request.method=="GET"):
-        rates=Rates.objects.all()
+
+        #to get distinct values ordering valueslist and distinct is needed based on which field needs to be distinct
+        #when flat is true, tuples are eliminated so [[1],[2]] would become [1,2]
+        rates=Rates.objects.order_by().distinct("quote_currency").values()
 
         # date=datetime.date(2024,5,28)
         # rates=Rates(date=date,quote_currency="LKR",exchange_rate=298.3)
         # rates.save()
-
-        serializedRates=serializers.serialize("json",rates)
-        return JsonResponse(json.loads(serializedRates),safe=False)
+        print(rates)
+        
+        # serializedRates=serializers.serialize("json",rates)
+        # print(serializedRates)
+        return JsonResponse(list(rates),safe=False)
     
 @api_view(["GET"])
 def viewHistoricalRates(request,quoteCurrency):
     if (request.method=="GET"):
-        formattedQuoteCurrency=quoteCurrency.upper() #entry of lkr would be taken as LKR
 
-        date=datetime.datetime.now()-datetime.timedelta(days=30) #taking the current date and going back 30 days
-        historicalRates=Rates.objects.filter(date__gt=date).filter(quote_currency=formattedQuoteCurrency) #using __greaterthan to filter dates greater than the date that was calculated 30 days ago
+        #entry of lkr would be taken as LKR
+        formattedQuoteCurrency=quoteCurrency.upper()
 
-        return JsonResponse(json.loads(serializers.serialize("json",historicalRates)),safe=False) #serializing data to convert from queryset to json and parsing the string object to an object to be displayed
+        #taking the current date and going back 30 days
+        date=datetime.datetime.now()-datetime.timedelta(days=30)
+
+        #using __greaterthan to filter dates greater than the date that was calculated 30 days ago
+        #you can import "Q" from django models and this basically allows you to do OR operations
+        #at the end of the operation add .values() so that the model instances arent includes which means no metadata included in result
+        historicalRates=Rates.objects.filter(date__gt=date).filter(quote_currency=formattedQuoteCurrency).values() 
+
+        print(historicalRates)
+        #serializing data to convert from queryset to json and parsing the string object to an object to be displayed
+        return JsonResponse(list(historicalRates),safe=False)
 
 @api_view(["GET"])
 def currentExchangeRate(request,quoteCurrency):
     if (request.method=="GET"):
+        
         # rates=Rates.objects.filter(quote_currency=quote_currency).aggregate(Max("exchange_rate")) this will get you the max value of a certain field
 
         formattedQuoteCurrency=quoteCurrency.upper() #entry of lkr would be taken as LKR
@@ -44,6 +55,6 @@ def currentExchangeRate(request,quoteCurrency):
         #aggregate doesnt return a queryset it returns a dictionary and you have to use dictionaryname.get("max_field") like aggregate(max_field=Max("date"))
         latestDate=Rates.objects.filter(quote_currency=formattedQuoteCurrency).aggregate(Max("date"))
         
-        latestRate=Rates.objects.filter(date=latestDate.get("date__max")).filter(quote_currency=formattedQuoteCurrency)
+        latestRate=Rates.objects.filter(date=latestDate.get("date__max")).filter(quote_currency=formattedQuoteCurrency).values()
    
-        return JsonResponse(json.loads(serializers.serialize("json",latestRate)),safe=False)
+        return JsonResponse(list(latestRate),safe=False)
